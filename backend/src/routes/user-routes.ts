43f5e45055ce319e4c40aac81f046d72119bbc49
linhpn <phanphanvjc@gmail.com>
2024-03-09 15:24:09 +0700

    feat: login and register with basic authentication

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,9 +1,12 @@
+import { validate, signupValidator, loginValidator } from '../utils/validators.js';
 
 import { Router } from "express";
-import { getAllUsers } from "../controllers/user-controllers.js";
+import { getAllUsers, userLogin, userSignup } from "../controllers/user-controllers.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers)
+userRoutes.post("/signup", await validate(signupValidator), userSignup)
+userRoutes.post("/login", await validate(loginValidator), userLogin)
 
 export default userRoutes;
