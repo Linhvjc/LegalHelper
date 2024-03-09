396c75263d1fdd4ab6710840557869bc43478129
linhpn <phanphanvjc@gmail.com>
2024-03-09 16:13:10 +0700

    feat: add token and cookie for authentication

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from 'dotenv';
 import morgan from 'morgan'
 import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
 config();
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 //! middlewares
 // process the json type
 app.use(express.json())
+app.use(cookieParser(process.env.COOKIE_SECRET))
 
 // remove it in production, it using for logging
 app.use(morgan("dev"))
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,8 @@
+import { COOKIE_NAME } from '../utils/constants.js'
 import {Request , Response, NextFunction } from "express"
 import User from "../models/User.js"
 import { hash, compare } from 'bcrypt';
+import { createToken } from "../utils/token-manager.js";
 
 export const getAllUsers = async (
     req: Request,
@@ -29,6 +31,26 @@ export const userSignup = async (
         const hashedPassword = await hash(password, 10)
         const user = new User({ name, email, password: hashedPassword })
         await user.save();
+
+        // Create token and store cokie
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/"
+        })
+
+        const token = createToken(user.id.toString(), user.email, "7d")
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7)
+        res.cookie(COOKIE_NAME, token, {
+            path: "/",
+            domain: "localhost",
+            expires,
+            httpOnly: true,
+            signed: true
+        })
+
         return res.status(201).json({message: "Ok", id:user._id.toString()})
     } catch (error) {
         console.log(error)
@@ -50,6 +72,25 @@ export const userLogin = async (
         if (!isPasswordCorrect) {
             return res.status(403).send("Incorrect Password")
         }
+
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/"
+        })
+
+        const token = createToken(user.id.toString(), user.email, "7d")
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7)
+        res.cookie(COOKIE_NAME, token, {
+            path: "/",
+            domain: "localhost",
+            expires,
+            httpOnly: true,
+            signed: true
+        })
+
 
         return res.status(201).json({message: "Ok", id:user._id.toString()})
     } catch (error) {
